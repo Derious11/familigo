8463da0feb4d40285c7533955f70f4a4541c78c1
Derious11 <derious11@gmail.com>
2025-11-18 19:24:00 -0700

    fix: correct storageBucket URL in Firebase configuration files

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBjrXSbC6tb8YBRnV7NWF3dBZH2r-v-pBY",
   authDomain: "familigo-11643.firebaseapp.com",
   projectId: "familigo-11643",
-  storageBucket: "familigo-11643.firebasestorage.app",
+  storageBucket: "familigo-11643.appspot.com",
   messagingSenderId: "34674887836",
   appId: "1:34674887836:web:bff36d1b66d97404dab159",
   measurementId: "G-5XW9XNW42X"
